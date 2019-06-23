54e20d73060d334e261964317a92b878d2f56050
tubidge <tubidge@gmail.com>
2019-06-23 10:48:43 -0600

    more readme

--- src/components/Jumbotron/index.js
+++ src/components/Jumbotron/index.js
@@ -4,7 +4,7 @@ function Jumbotron() {
     return (
         <div className="jumbotron">
             <h4>Game Instructions</h4>
-            <p>Your goal is to click on each image one time and one time only. After each click, the images will be shuffled. It's your job to remember which images you have or have not clicked.</p>
+            <p>Your goal is to click on each image one time and one time only. After each click, the images will be shuffled. It's your job to remember which images you have or have not clicked. If you click on the same image twice, the game will reset.</p>
         </div>
     );
 };
