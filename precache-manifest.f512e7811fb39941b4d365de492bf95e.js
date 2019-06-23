8a9b3d39b711150f82b4fd3a99e8b08a1ee207fb
tubidge <tubidge@gmail.com>
2019-06-23 10:59:37 -0600

    Updates

---
+++ precache-manifest.f512e7811fb39941b4d365de492bf95e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dc1a19c0fab3ae25e9494a2302ffb75",
+    "url": "/react-memory/index.html"
+  },
+  {
+    "revision": "3e7ef30a283847565333",
+    "url": "/react-memory/static/css/main.7f43f243.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/react-memory/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "3e7ef30a283847565333",
+    "url": "/react-memory/static/js/main.022f4537.chunk.js"
+  },
+  {
+    "revision": "591630390940aacf310a",
+    "url": "/react-memory/static/js/runtime~main.fbbade95.js"
+  }
+]);
